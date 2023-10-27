cddc165995da7cedb6935f64ccd7b4a9ffdd8463
EnesBrt <enes.barut@gmx.com>
2023-10-27 16:20:36 +0200

    added models views and admin super user

--- enes_journals/my_journals/models.py
+++ enes_journals/my_journals/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+class Articles(models.Model):
+    headline = models.CharField(max_length=255)
+    content = HTMLField()
+    date_of_publication = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.headline
--- enes_journals/my_journals/views.py
+++ enes_journals/my_journals/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Articles
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def list_articles(request):
+    articles = Articles.objects.all()
+    return render(request, 'list_articles.html', {'articles': articles})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Articles, pk=article_id)
+    return render(request, 'article_detail.html', {'article': article})

7afdb3a674d3b4fa5338314f2ab705c424669369
EnesBrt <enes.barut@gmx.com>
2023-10-30 18:34:44 +0100

    Added cards for atricles and a button in landing page

--- enes_journals/enes_journals/urls.py
+++ enes_journals/enes_journals/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('', include('my_journals.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

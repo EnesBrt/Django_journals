16a8718a9c71459fbbc679cf33c940bfa51dc4fe
EnesBrt <enes.barut@gmx.com>
2023-10-31 17:04:26 +0100

    tested the code with unittest

--- enes_journals/my_journals/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Articles
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('headline', 'date_of_publication')
-    search_fields = ('headline',)
-
-admin.site.register(Articles, ArticleAdmin)
--- enes_journals/my_journals/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(label='nom et prénom', max_length=100)
-    subject = forms.CharField(label='Objet', max_length=100)
-    message = forms.CharField(label='Message', widget=forms.Textarea)
-    
-    
